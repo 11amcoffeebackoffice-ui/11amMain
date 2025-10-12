81d2d5c372029d2bad1a17f019d014be64a55791
11amcoffeebackoffice-ui <11amcoffeebackoffice@gmail.com>
2025-10-12 23:50:14 +0800

    fix resize problem

--- script.js
+++ script.js
@@ -488,4 +488,21 @@
 	document.addEventListener('DOMContentLoaded', () => {
 	  new FeedbackForm();
 	});
+	
+		// Prevent pinch zoom
+	  document.addEventListener('touchstart', function (event) {
+		if (event.touches.length > 1) {
+		  event.preventDefault();
+		}
+	  }, { passive: false });
+
+	  // Prevent double-tap zoom
+	  let lastTouchEnd = 0;
+	  document.addEventListener('touchend', function (event) {
+		const now = new Date().getTime();
+		if (now - lastTouchEnd <= 300) {
+		  event.preventDefault();
+		}
+		lastTouchEnd = now;
+	  }, false);
 	  
